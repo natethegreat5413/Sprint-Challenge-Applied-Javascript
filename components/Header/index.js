8a1961295ecc53ff3ad047c21784cbb62e23ca75
Nathan Cowley <nathancowley12@gmail.com>
2020-04-10 09:36:02 -0600

    done with the header except for the temp and date. still figuring that out but moving on for now

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,39 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+axios
+    .get("https://unpkg.com/axios/dist/axios.min.js")
+    .then((response) => {
+        let data = response.data
+        headerC.appendChild(header(data))
+        
+    })
+
+// HEADER FUNCTION
+
+const headerC = document.querySelector('.header-container')
+
+function header(data) {
+    const heading = document.createElement('div')
+    const date = document.createElement('span')
+    const title = document.createElement('h1')
+    const temp = document.createElement('span')
+
+    heading.appendChild(date)
+    heading.appendChild(title)
+    heading.appendChild(temp)
+
+    heading.classList.add('header')
+    date.classList.add('date')
+    temp.classList.add('temp')
+
+    date.textContent = `${data.date}`
+    temp.textContent = `${data.temp}`
+    title.textContent = 'Lambda Times'
+
+
+    return heading
+
+    
+
+}
